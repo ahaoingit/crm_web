8929b845575b37916dd8ad581e79e6d28c315b4e
latelatechoco <1812305828@qq.com>
2020-12-06 14:00:53 +0800

    部门管理新页面，编辑弹窗

--- js/ahao.js
+++ js/ahao.js
@@ -1,1 +1,15 @@
 const serverUrl = "http://127.0.0.1:8099"
+
+
+function convertNum(allDepartmentData) {
+	var departMap = new Map();
+	departMap.set(0,"公司");
+	for(index in allDepartmentData) {
+		departMap.set(allDepartmentData[index].did,allDepartmentData[index].dName);
+	}
+	for(index in allDepartmentData) {
+		allDepartmentData[index].dParentId = departMap.get(allDepartmentData[index].dParentId);
+	}
+	
+	return allDepartmentData;
+}
